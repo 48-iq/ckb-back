849482178307571ee908f127398aa345641f155e
48-iq <ivn25ov@mail.ru>
2025-12-18 10:36:29 +0400

    initial commit

---
+++ src/chat/chat.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get } from "@nestjs/common";
+
+
+@Controller()
+export class ChatController {
+
+  constructor() {}
+
+  @Get()
+  allChatsOfUser() {
+    
+  }
+
+}
---
+++ src/chat/chat.dto.ts
@@ -0,0 +1,5 @@
+
+export class ChatDto {
+  id: string
+  title: string
+}
