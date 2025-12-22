92cacd5488a8a70903473e57f41cefa323fa7a12
48-iq <ivn25ov@mail.ru>
2025-12-22 06:38:06 +0400

    save progress

--- src/agent/agent.service.ts
+++ src/agent/agent.service.ts
@@ -1,22 +1,31 @@
-import { ToolNode } from "@langchain/langgraph/prebuilt";
-import { Inject, Injectable } from "@nestjs/common";
-import { GigaChat } from "langchain-gigachat";
-import { DynamicTool } from "langchain/tools";
+import { Inject, Injectable } from "@nestjs/common"
+import { AGENT_NODE } from "./nodes/agent-node.provider"
+import { DOCUMENT_NODE } from "./nodes/document-node.provider"
+import { StateGraph, START, END } from "@langchain/langgraph"
+import { State } from "./agent.state"
+import { RESULT_NODE } from "./nodes/result-node.provider"
+import { PLAN_NODE } from "./nodes/plan-node.provider"
 
 
 @Injectable()
 export class AgentService {
 
   constructor(
-    @Inject('AGENT_MODEL') private readonly model: GigaChat,
-    @Inject('AGENT_TOOLS') private readonly tools: DynamicTool[]
+    @Inject(AGENT_NODE) private readonly agentNode,
+    @Inject(DOCUMENT_NODE) private readonly documentNode,
+    @Inject(RESULT_NODE) private readonly resultNode,
+    @Inject(PLAN_NODE) private readonly planNode
   ) 
   {
-    model.bindTools(tools)
-    this.toolNode = new ToolNode(tools)
-  }
+    const agent = new StateGraph(State)
+      .addNode("agentNode", agentNode)
+      .addNode("planNode", planNode)
+      .addNode("resultNode", resultNode)
+      .addNode("documentNode", documentNode)
+      .addEdge(START, "agentNode")
+      .addEdge(documentNode, END);
 
-  private readonly toolNode: ToolNode
+  }
 
   
 
